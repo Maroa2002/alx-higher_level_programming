8afe4ecc3c6e37b10c9b25fe6050dcc9b97376e5
Maroa2002 <mgm.engineeringtie847@gmail.com>
2023-09-07 14:01:00 +0300

    task 2 modified

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,9 +5,12 @@ if __name__ == "__main__":
     num_args = len(argv)
     args_list = argv
 
-    if num_args <= 1:
+    if num_args == 1:
         print(f"{num_args - 1} arguments.")
+    elif num_args == 2:
+        print(f"{num_args - 1} argument:")
     else:
         print(f"{num_args - 1} arguments:")
-        for i in range(1, num_args):
-            print(f"{i}: {args_list[i]}")
+
+    for i in range(1, num_args):
+        print(f"{i}: {args_list[i]}")
