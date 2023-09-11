cfd1413e16f3e2273938f4ebf50c480201676c42
Maroa2002 <mgm.engineeringtie847@gmail.com>
2023-09-11 07:44:32 +0300

    solved the code fail

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0:
+    if idx < 0 or idx > len(my_list) - 1:
         return None
 
-    if idx >= len(my_list):
-        return None
-
-    element = "{:d}".format(my_list[idx])
+    element = my_list[idx]
     return element
