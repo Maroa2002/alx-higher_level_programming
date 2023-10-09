798a704737b5c718b7622f8a7ab3b40ea0d38815
Maroa2002 <mgm.engineeringtie847@gmail.com>
2023-10-09 23:55:15 +0300

    class Ssssssquare that inherits from Rectangle

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -12,3 +12,7 @@ class Square(Rectangle):
         super().__init__(size, size)
         self.integer_validator = ("size", size)
         self.__size = size
+
+    def area(self):
+        """returns the area"""
+        return self.__size ** 2
