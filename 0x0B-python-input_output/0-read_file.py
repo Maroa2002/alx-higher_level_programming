0634ca1fa42a5eda07d738e55428809bee9f43da
Maroa2002 <mgm.engineeringtie847@gmail.com>
2023-10-10 15:31:05 +0300

    reads a text file and prints to stdout

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,4 +8,4 @@ def read_file(filename=""):
     """reads file and prints to stdout"""
     with open(filename, 'r', encoding='utf-8') as file:
         data_read = file.read()
-        print(data_read)
+        print(data_read, end="")
