c64bd2a1a78f5d3c96fb489b98f07f0ce6a4739f
Maroa2002 <mgm.engineeringtie847@gmail.com>
2023-10-11 05:12:14 +0300

    a function that returns the dictionary description with simple data structure (list, dictionary, string, integer and boolean) for JSON serialization of an object

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -6,15 +6,5 @@ contains the function class_to_json
 
 def class_to_json(obj):
     """returns the dictionary description with simple data structure"""
-    if isinstance(obj, (int, bool, str)):
-        # return object itself if a basic data type
-        return obj
-    elif isinstance(obj, list):
-        # simplify each element if object is a list
-        return [class_to_json(item) for item in obj]
-    elif isinstance(obj, dict):
-        # simplify keys and values if a dictionary
-        return {str(key): class_to_json(value) for key, value in obj.items()}
-    else:
-        # convert to string representation for unknown type
-        return str(obj)
+    return {key: value for (key, value) in obj.__dict__.items()
+            if key in list(obj.__dict__.keys())}
