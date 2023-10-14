27ba33efbbdc803c4682b149fe1c0813403bc631
Maroa2002 <mgm.engineeringtie847@gmail.com>
2023-10-14 07:18:45 +0300

    [200~Update the class Rectangle by adding validation of all setter methods and instantiation (id excluded)

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -11,12 +11,12 @@ class Rectangle(Base):
     """Class Rectangle inherits from Base"""
     def __init__(self, width, height, x=0, y=0, id=None):
         """class constructor"""
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
         # Call the super class with id
         super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
@@ -41,21 +41,43 @@ class Rectangle(Base):
     @width.setter
     def width(self, width):
         """width setter"""
-        if width > 0:
-            self.__width = width
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+
+        if width <= 0:
+            raise ValueError("width must be > 0")
+
+        self.__width = width
 
     @height.setter
     def height(self, height):
         """height setter"""
-        if height > 0:
-            self.__height = height
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+
+        if height <= 0:
+            raise ValueError("height must be > 0")
+
+        self.__height = height
 
     @x.setter
     def x(self, x):
         """x setter"""
+        if type(x) is not int:
+            raise TypeError("x must be an integer")
+
+        if x < 0:
+            raise ValueError("x must be >= 0")
+
         self.__x = x
 
     @y.setter
     def y(self, y):
         """y setter"""
+        if type(y) is not int:
+            raise TypeError("y must be an integer")
+
+        if y < 0:
+            raise ValueError("y must be >= 0")
+
         self.__y = y
