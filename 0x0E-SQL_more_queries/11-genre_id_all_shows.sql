67ab5bb064ed796324f85ba61c484ca3967c36c3
Maroa2002 <mgm.engineeringtie847@gmail.com>
2024-01-17 16:35:41 +0300

    left join

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,5 @@
 -- Import the database dump from hbtn_0d_tvshows to your MySQL server
--- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
+-- lists all shows contained in hbtn_0d_tvshows
 -- Each record should display: tv_shows.title - tv_show_genres.genre_id
 -- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 -- If a show doesn’t have a genre, display NULL
