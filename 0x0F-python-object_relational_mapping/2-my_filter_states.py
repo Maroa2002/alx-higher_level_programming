81ab27ab58657803c54408ec12b3c8ff965beb4b
Maroa2002 <mgm.engineeringtie847@gmail.com>
2024-02-16 23:26:12 +0300

    filtering states by user input

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -19,6 +19,7 @@ if __name__ == "__main__":
                 WHERE name LIKE '{}' ORDER BY id ASC".format(argv[4]))
     rows = cur.fetchall()
     for row in rows:
-        print(row)
+        if row[1] == argv[4]:
+            print(row)
     cur.close()
     db.close()
