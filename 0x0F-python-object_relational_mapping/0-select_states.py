8a046e7875ccd0711317534014bef14a68597a13
Maroa2002 <mgm.engineeringtie847@gmail.com>
2024-02-16 21:29:48 +0300

    listing all states in a database

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""listing all states from a database"""
+
+import MySQLdb
+
+db = MySQLdb.connect(host='localhost',
+                     port=3306,
+                     user='root',
+                     passwd='#Ombudsman10/',
+                     db='hbtn_0e_0_usa')
+cur = db.cursor()
+cur.execute('SELECT * FROM states')
+rows = cur.fetchall()
+for row in rows:
+    print(row)
+cur.close()
+db.close()
